0cc0af4fdc239e7529c583baa9d83cd4b096b720
desdrury <des@drury-family.com>
2016-08-10 10:30:33 +1000

    Certificate generation working

--- utilities/upgrade.sh
+++ utilities/upgrade.sh
@@ -3,4 +3,4 @@
 INVENTORY=${INVENTORY:-../.vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory}
 
 #ansible-playbook ../site.yml 
-ansible k8sdc-master -m setup -i $INVENTORY
+ansible k8sdc-masters -m setup -i $INVENTORY
